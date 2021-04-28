335852f95eec165ef72f83ccc4aa4dacd574db2a
Dima Alekseev <alekseevda24@gmail.com>
2021-04-28 13:46:05 +0400

    Add examples for different types of operators

---
+++ baseElements/examples/ternaryOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,max;
+
+    cin >> x >> y;
+    (x > y) ? cout << x: cout << y << endl;
+    max = (x > y) ? x : y;
+    cout << max << endl;
+
+    return 0;
+}
