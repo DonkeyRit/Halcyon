19eaa8c7a0345b45b75eebdea88991b25bd9662f
Dima Alekseev <alekseevda24@gmail.com>
2021-05-02 14:26:09 +0400

    Add chess elephant moves task from Stepik

---
+++ operators/stepik/chessElephantMoves.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Шахматный слон ходит по диагонали. Даны две различные клетки шахматной доски, 
+определите, может ли слон попасть с первой клетки на вторую одним ходом.
+
+Формат входных данных
+    Программа получает на вход четыре числа от 1 до 8 каждое, задающие номер столбца 
+    и номер строки сначала для первой клетки, потом для второй клетки.
+Формат выходных данных
+    Программа должна вывести YES, если из первой клетки ходом слона можно попасть 
+    во вторую или NO в противном случае.
+*/
+
+int main()
+{
+    int startX, startY, targetX, targetY;
+    cin >> startX >> startY >> targetX >> targetY;
+
+    bool mainDiagonal = (startX - targetX) == (startY - targetY);
+    bool sideDiagonal = -1 * (startX - targetX) == (startY - targetY) || (startX - targetX) == -1 * (startY - targetY);
+
+    if(mainDiagonal || sideDiagonal)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
